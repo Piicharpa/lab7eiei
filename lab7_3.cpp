ab5d293b7579a32fff6a9f51c1d8ce226e4c10e5
piicharpa <neuah2515@gmail.com>
2022-12-13 17:18:29 +0700

    lab3complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,6 +1,16 @@
-#include<iostream>
 
+
+#include<iostream>
+#include<cmath>
 using namespace std;
+
+int adiff(int A, int B){
+    int d = abs(A-B)%360;
+        if(d>180){
+            d = 360-d;
+        }
+    return d ;
+}
 
 
 int main(){
